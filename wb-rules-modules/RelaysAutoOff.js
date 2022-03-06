3df48784df4867b1b5846d557952784384f40c81
imposibrus <imposibrus@gmail.com>
2022-03-06 19:31:13 +0300

    RelaysAutoOff - refactor to use PersistentStorage

--- wb-rules-modules/RelaysAutoOff.js
+++ wb-rules-modules/RelaysAutoOff.js
@@ -1,44 +1,82 @@
-/* globals defineRule, defineVirtualDevice, log, global, dev, exports, cron */
+/* globals defineRule, defineVirtualDevice, log, global, dev, exports, cron, PersistentStorage */
 
 var virtualDeviceName = "overheat_control";
+var virtualDeviceControlName = virtualDeviceName + "/enabled";
 var autoOffTimeout = 1000 * 60 * 30; // 30 minutes
+/** @type Object.<string, number> */
+var lastRelayActivationTime = new PersistentStorage('RelaysAutoOff');
+
+defineVirtualDevice(virtualDeviceName, {
+  title: "Управление перегревом",
+  cells: {
+    enabled: {
+      type: "switch",
+      value: true
+    }
+  }
+});
 
 var RelaysAutoOff = {
   /**
    * @param {String} relayDev
    */
   addControl: function addControl(relayDev) {
-    var timer;
+    var ruleName = 'RelaysAutoOff_' + relayDev.replace('/', '_');
+    log('RelaysAutoOff::addControl relayDev="{}", ruleName="{}"'.format(relayDev, ruleName));
 
-    defineRule({
-      whenChanged: relayDev,
-      then: function (newVal) {
-        if (newVal && dev[virtualDeviceName] && !timer) {
-          log('RelaysAutoOff:: staring timer of relay "{}"'.format(relayDev));
-          timer = setTimeout(function() {
-            log('RelaysAutoOff:: timer of relay "{}" finished. turn off the relay'.format(relayDev));
-            dev[relayDev] = false;
-            timer = undefined;
-          }, autoOffTimeout);
-        } else if (timer) {
-          log('RelaysAutoOff:: clearing timer of relay "{}"'.format(relayDev));
-          clearTimeout(timer);
+    RelaysAutoOff.relaysUnderControl.push(relayDev);
+    lastRelayActivationTime[relayDev] = 0;
+
+    defineRule(ruleName, {
+      whenChanged: [relayDev, virtualDeviceControlName],
+      then: function () {
+        if (dev[relayDev] && dev[virtualDeviceControlName]) {
+          log('RelaysAutoOff:: relay "{}" activated, saving activation time'.format(relayDev));
+
+          lastRelayActivationTime[relayDev] = (new Date()).getTime();
+        } else {
+          log('RelaysAutoOff:: relay "{}" deactivated, unset activation time'.format(relayDev));
+
+          lastRelayActivationTime[relayDev] = 0;
         }
       }
     });
   },
 
   init: function init() {
-    defineVirtualDevice(virtualDeviceName, {
-      title: "Управление перегревом",
-      cells: {
-        enabled: {
-          type: "switch",
-          value: true
-        }
+    defineRule({
+      when: cron("@every 1m"),
+      then: function() {
+        var now = new Date();
+        var nowTimestamp = now.getTime();
+
+        RelaysAutoOff.relaysUnderControl.forEach(function(relayDev) {
+          var relayTurnOnTime = lastRelayActivationTime[relayDev];
+
+          // activation time not set, ignore
+          if (!relayTurnOnTime) {
+            return;
+          }
+
+          // relay turned off, unset activation time and ignore
+          if (!dev[relayDev]) {
+            log('RelaysAutoOff::cron relay "{}" already turned off. unset activation time and ignore'.format(relayDev));
+            lastRelayActivationTime[relayDev] = 0;
+            return;
+          }
+
+          // relay activation time reached `autoOffTimeout`, turning off
+          if (relayTurnOnTime + autoOffTimeout < nowTimestamp) {
+            log('RelaysAutoOff::cron relay "{}" was turned ON at "{}" and its activation time reached `autoOffTimeout` at "{}", turning relay off'.format(relayDev, new Date(relayTurnOnTime).toISOString(), new Date(relayTurnOnTime + autoOffTimeout).toISOString()));
+            dev[relayDev] = false;
+            lastRelayActivationTime[relayDev] = 0;
+          }
+        });
       }
     });
-  }
+  },
+
+  relaysUnderControl: []
 };
 
 exports.RelaysAutoOff = RelaysAutoOff;
--- wb-rules/relay_auto_off.js
+++ wb-rules/relay_auto_off.js
@@ -1,3 +1,5 @@
+/* globals defineRule, defineVirtualDevice, log, global, dev, exports, cron, PersistentStorage */
+
 var RelaysAutoOff = require('RelaysAutoOff').RelaysAutoOff;
 
 RelaysAutoOff.addControl('wb-mrm2-mini_134/Relay 1');
